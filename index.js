9587d87c0644c8a8909ef4915c19e5952ca064b4
ravi yadav <raviyadav00063@gmail.com>
2020-05-22 19:41:35 +0530

    Initial commit

--- index.js
+++ index.js
@@ -53,31 +53,105 @@
 
 //URL BUILDING
 
+// const express = require("express");
+// const app = express();
+
+// app.get("/", function (req, res) {
+//   res.send("Hello from ExpressJS Website.");
+// });
+
+// app.get("/profile/:id", function (req, res) {
+//   res.send("Profile ID: " + req.params.id);
+// });
+
+// app.get("/:course_name/courses/:id", function (req, res) {
+//   res.send("Course Name: " + req.params.course_name + ", ID: " + req.params.id);
+// });
+
+// app.get("/message/:phone_number([0-9]{10})", function (req, res) {
+//   res.send("Message To " + req.params.phone_number);
+// });
+
+// //NOTE: Expressjs always reads the file from top to bottom
+// //so always write the "*" code at the end of the code otherwise
+// //it will always show Page Not Found
+
+// app.get("*", function (req, res) {
+//   res.send("Page Not Found....");
+// });
+
+// app.listen(100);
+
+// // NOTE: Install Pug it is a template engine used in expressjs
+
+// const express = require("express");
+// const app = express();
+
+// function logInfo(req, res, next) {
+//   console.log("Hello from logInfo() Middleware....");
+//   next();
+// }
+
+// function addData(req, res, next) {
+//   var person = {
+//     name: "Sandeep Ganguly",
+//     city: "Kanpur",
+//   };
+//   req.person = person;
+//   next();
+// }
+
+// app.use(logInfo);
+// app.use(addData);
+
+// app.use("/profile/:id([0-3]{1})", function (req, res, next) {
+//   var names = ["Sandeep", "Amit", "Deepak", "Aman"];
+//   req.name = names[req.params.id];
+//   next();
+// });
+
+// app.get("/", function (req, res) {
+//   res.send("Welcome to Index page....");
+// });
+
+// app.get("/person", function (req, res) {
+//   res.send("Name: " + req.person.name + ", City: " + req.person.city);
+// });
+
+// app.get("/profile/:id([0-3]{1})", function (req, res) {
+//   res.send("Name: " + req.name);
+// });
+
+// app.listen(80);
+
+//NOTE: First HTML Page using PUG
+
 const express = require("express");
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// app.get("/", function (req, res) {
+//   res.render("example", {
+//     //Passing Values using Javascript Object into PUG File
+//     name: "Sandeep Ganguly",
+//     url: "https://www.facebook.com",
+//     skills: ["Android", "ExpressJS", "Vue.JS"],
+//   });
+// });
+
+//NOTE: If-Else Condition in PUG
+
 app.get("/", function (req, res) {
-  res.send("Hello from ExpressJS Website.");
+  var person = {
+    name: "Ravi Yadav",
+    city: "Kanpur",
+    tutorial: "ExpressJS",
+  };
+  res.render("example", {
+    data: person,
+  });
 });
 
-app.get("/profile/:id", function (req, res) {
-  res.send("Profile ID: " + req.params.id);
-});
-
-app.get("/:course_name/courses/:id", function (req, res) {
-  res.send("Course Name: " + req.params.course_name + ", ID: " + req.params.id);
-});
-
-app.get("/message/:phone_number([0-9]{10})", function (req, res) {
-  res.send("Message To " + req.params.phone_number);
-});
-
-//NOTE: Expressjs always reads the file from top to bottom
-//so always write the "*" code at the end of the code otherwise
-//it will always show Page Not Found
-
-app.get("*", function (req, res) {
-  res.send("Page Not Found....");
-});
-
-app.listen(100);
+app.listen(80);
